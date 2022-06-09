3e5d3b6714be104a4c37f24c1c1a404789b0c55b
Upper <goodp@yandex.ru>
2022-06-09 11:36:16 +0300

    ex 2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int k = 0; k < matrix.GetLength(1); k++)
+            matrix[i,k] = i+k;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+            Console.Write(matrix[i,k] + " ");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Enter the number of lines: ");
+int rows = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine("Enter the number of columnes: ");
+int columnes = int.Parse(Console.ReadLine()?? "");
+
+int[,] matrix = new int[rows,columnes];
+
+FillArray(matrix);
+PrintArray(matrix);
